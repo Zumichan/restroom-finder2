c0de025bec4394a41fd075745241fe20b15c622f
zumichan <azumi.y.callanan@gmail.com>
2018-12-19 16:23:39 -0800

    Implemented user authorization

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -4,6 +4,12 @@ module.exports = {
     const restroomRoutes = require("../routes/restrooms");
     const searchRoutes = require("../routes/search");
     const userRoutes = require("../routes/users");
+
+    if(process.env.NODE_ENV === "test") {
+      const mockAuth = require("../../spec/support/mock-auth.js");
+      mockAuth.fakeIt(app);
+    }
+
     app.use(staticRoutes);
     app.use(restroomRoutes);
     app.use(searchRoutes);
